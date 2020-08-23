6a5cdfa78a7ab1111b619435e19fbb632f06b2e7
Nils Pettersson <nils-pettsson@outlook.com>
2020-08-23 11:28:51 +0200

    added solve funtion that can find solutions for sudoku

--- main.js
+++ main.js
@@ -10,7 +10,11 @@ var array =[[0,0,0,0,0,0,0,0,0],
 
 console.log(array);
 
-possible(0, 0, 1);
+var solutions = 0;
+
+solve(array);
+
+//possible(0, 0, 1);
 
 function possible(x, y, num){
     //check vertical.
@@ -25,7 +29,7 @@ function possible(x, y, num){
             return false;
         }
     }
-    //finding the column and row the point is in
+    //finding the column and row the point is in.
     var xColumn = Math.floor(x / 3);
     var yColumn = Math.floor(y / 3);
     for(var i = yColumn * 3; i <  yColumn * 3 + 3; i++){
@@ -35,4 +39,40 @@ function possible(x, y, num){
             }
         }
     }
+
+    return true;
+}
+
+
+function solve(){
+    if(solutions >= 1){
+        return;
+    }
+
+    //loop through grid.
+    for(var y = 0; y <  array.length; y++){
+        for(var x = 0; x < array[y].length; x++){
+            if(array[y][x] != 0){
+                continue;
+            }
+            for(var n = 1; n < 10; n++){
+                if(possible(x, y, n)){
+                    array[y][x] = n;
+                    solve();
+                    //array[y][x] = 0;
+                    if(solutions < 1){
+                        array[y][x] = 0;
+                    }
+                    /*if(solutionFound == false){
+                        solution[y][x] = 0;
+                    }*/
+                }
+            }
+            return;
+
+        }
+    }
+    solutions++;
+    console.log(array);
+    
 }
