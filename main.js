e86163d9d14f3bc1d053243f1de86c53d431d07f
Nils Pettersson <nils-pettsson@outlook.com>
2020-08-23 10:25:45 +0200

    added a function to check if a number in some location is valid

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var array = [[0,0,0,0,0,0,0,0,0],
+var array =[[0,0,0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0,0,0],
@@ -10,4 +10,29 @@ var array = [[0,0,0,0,0,0,0,0,0],
 
 console.log(array);
 
+possible(0, 0, 1);
 
+function possible(x, y, num){
+    //check vertical.
+    for(var i = 0; i < array.length; i++){
+        if(array[i][x] == num){
+            return false;
+        }
+    }
+    //check horizontal.
+    for(var i = 0; i < array[y].length; i++){
+        if(array[y][i] == num){
+            return false;
+        }
+    }
+    //finding the column and row the point is in
+    var xColumn = Math.floor(x / 3);
+    var yColumn = Math.floor(y / 3);
+    for(var i = yColumn * 3; i <  yColumn * 3 + 3; i++){
+        for(var j = xColumn * 3; j < xColumn * 3 + 3; j++){
+            if(array[i][j] == num){
+                return false;
+            }
+        }
+    }
+}
