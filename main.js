4295ff585f5dad81d6fb130b62874c0dc34b3257
Nils Pettersson <nils-pettsson@outlook.com>
2020-08-23 11:36:34 +0200

    you can now set the amount of solutions to be found

--- main.js
+++ main.js
@@ -8,13 +8,11 @@ var array =[[0,0,0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0,0,0]];
 
-console.log(array);
 
 var solutions = 0;
 
-solve(array);
+solve(2);
 
-//possible(0, 0, 1);
 
 function possible(x, y, num){
     //check vertical.
@@ -44,11 +42,10 @@ function possible(x, y, num){
 }
 
 
-function solve(){
-    if(solutions >= 1){
+function solve(maxSolutions){
+    if(solutions >= maxSolutions){
         return;
     }
-
     //loop through grid.
     for(var y = 0; y <  array.length; y++){
         for(var x = 0; x < array[y].length; x++){
@@ -58,21 +55,23 @@ function solve(){
             for(var n = 1; n < 10; n++){
                 if(possible(x, y, n)){
                     array[y][x] = n;
-                    solve();
-                    //array[y][x] = 0;
-                    if(solutions < 1){
+                    solve(maxSolutions);
+                    if(solutions < maxSolutions){
                         array[y][x] = 0;
                     }
-                    /*if(solutionFound == false){
-                        solution[y][x] = 0;
-                    }*/
                 }
             }
             return;
-
+        }
+    }
+    var answer = [];
+    for(var y = 0; y <  array.length; y++){
+        answer.push([])
+        for(var x = 0; x < array[y].length; x++){
+            answer[y].push(array[y][x]);
         }
     }
     solutions++;
-    console.log(array);
+    console.log(answer);
     
 }
